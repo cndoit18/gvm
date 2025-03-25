4a6a7c8e378be7bf1e2e43fa5d93f3a3abd09f07
cndoit18 <cndoit18@outlook.com>
2025-03-25 19:15:39 +0800

    feat: remove Go setup step and add gvm bootstrap test script
    Change-Id: I57682bdcaed747d7560735354d050a6ed0f9059e

--- tests/00gvm_install_comment_test.sh
+++ tests/00gvm_install_comment_test.sh
@@ -1,8 +1,12 @@
 ## Cleanup test objects
 gvm uninstall go1.20.7 > /dev/null 2>&1
-gvm uninstall master > /dev/null 2>&1
 gvm uninstall go1.21.4 > /dev/null 2>&1
 gvm uninstall go1.23.6 > /dev/null 2>&1
+
+gvm uninstall go1.17.13 > /dev/null 2>&1
+gvm uninstall go1.20.6  > /dev/null 2>&1
+gvm uninstall go1.22.6  > /dev/null 2>&1
+gvm uninstall master    > /dev/null 2>&1
 #######################
 
 gvm install go1.20.7 #status=0
@@ -10,5 +14,11 @@ GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.20.7 gvm install go1.21.4 #status=0
 gvm list #status=0; match=/go1.21.4/
 GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.20.7 gvm install go1.23.6 #status=0
 gvm list #status=0; match=/go1.23.6/
-GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.23.6 gvm install master #status=0
+
+gvm install go1.17.13 -B #status=0
+GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.17.13 gvm install go1.20.6  #status=0
+gvm list #status=0; match=/go1.20.6/
+GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.20.6  gvm install go1.22.6  #status=0
+gvm list #status=0; match=/go1.22.6/
+GOROOT_BOOTSTRAP=$GVM_ROOT/gos/go1.22.6  gvm install master    #status=0
 gvm list #status=0; match=/master/
